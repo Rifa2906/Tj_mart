3c3aade7aa948ec9af37f1f90544a1eec181d1d6
rifa2906 <121f429@gmail.com>
2022-10-31 14:56:17 +0700

    commit database

--- tj_mart.sql
+++ tj_mart.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 26 Okt 2022 pada 10.37
+-- Waktu pembuatan: 31 Okt 2022 pada 08.55
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 7.4.29
 
@@ -29,11 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tb_barang_keluar` (
   `id_keluar` int(11) NOT NULL,
-  `kode_barang_keluar` varchar(10) NOT NULL,
-  `tanggal_keluar` date NOT NULL,
-  `id_barang` int(10) NOT NULL,
+  `id_barang` int(11) NOT NULL,
   `id_satuan` int(11) NOT NULL,
   `id_jenis` int(11) NOT NULL,
+  `tanggal_keluar` date NOT NULL,
   `jumlah` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,12 +40,12 @@ CREATE TABLE `tb_barang_keluar` (
 -- Dumping data untuk tabel `tb_barang_keluar`
 --
 
-INSERT INTO `tb_barang_keluar` (`id_keluar`, `kode_barang_keluar`, `tanggal_keluar`, `id_barang`, `id_satuan`, `id_jenis`, `jumlah`) VALUES
-(51, 'T-BK-0001', '2022-10-22', 12, 9, 1, 20),
-(52, 'T-BK-0002', '2022-11-15', 12, 9, 1, 40),
-(53, 'T-BK-0003', '2022-12-15', 12, 9, 1, 20),
-(54, 'T-BK-0004', '2023-01-07', 12, 9, 1, 15),
-(56, 'T-BK-0005', '2023-10-25', 13, 8, 1, 50);
+INSERT INTO `tb_barang_keluar` (`id_keluar`, `id_barang`, `id_satuan`, `id_jenis`, `tanggal_keluar`, `jumlah`) VALUES
+(75, 21, 9, 1, '2022-07-30', 100),
+(76, 21, 9, 1, '2022-08-30', 30),
+(77, 21, 9, 1, '2022-09-30', 15),
+(78, 22, 9, 1, '2022-10-31', 70),
+(79, 22, 9, 1, '2022-11-30', 25);
 
 -- --------------------------------------------------------
 
@@ -56,13 +55,12 @@ INSERT INTO `tb_barang_keluar` (`id_keluar`, `kode_barang_keluar`, `tanggal_kelu
 
 CREATE TABLE `tb_barang_masuk` (
   `id_masuk` int(11) NOT NULL,
-  `kode_barang_masuk` varchar(10) NOT NULL,
-  `tanggal_masuk` date NOT NULL,
   `id_barang` int(10) NOT NULL,
   `id_satuan` int(11) NOT NULL,
   `id_jenis` int(11) NOT NULL,
-  `jumlah` int(5) NOT NULL,
   `id_pemasok` int(11) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `jumlah` int(5) NOT NULL,
   `tanggal_kadaluarsa` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -109,11 +107,11 @@ CREATE TABLE `tb_kadaluarsa` (
 
 CREATE TABLE `tb_monitoring_kadaluarsa` (
   `id_monitoring` int(11) NOT NULL,
-  `tanggal_kadaluarsa` date NOT NULL,
   `id_barang` int(11) NOT NULL,
-  `jumlah` int(11) NOT NULL,
   `id_satuan` int(11) NOT NULL,
-  `id_jenis` int(11) NOT NULL
+  `id_jenis` int(11) NOT NULL,
+  `tanggal_kadaluarsa` date NOT NULL,
+  `jumlah` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -164,7 +162,8 @@ CREATE TABLE `tb_pengguna` (
 
 INSERT INTO `tb_pengguna` (`id_pengguna`, `nama`, `alamat`, `no_telpon`, `email`, `kata_sandi`, `hak_pengguna`) VALUES
 (1, 'Euis Mulyani', 'Cisitu', '028467352738', 'euis_mulyani@gmail.com', 'mulyani', 'kepala gudang'),
-(23, 'Hary', 'Bandung', '120394857321', 'hary@gmail.com', 'hary', 'staf gudang');
+(23, 'Hary', 'Bandung', '120394857321', 'hary@gmail.com', 'hary', 'staf gudang'),
+(24, 'Rifa', 'Tasikmalaya', '082315728130', 'rifa@gmail.com', 'rifa', 'staf administrasi');
 
 -- --------------------------------------------------------
 
@@ -174,18 +173,53 @@ INSERT INTO `tb_pengguna` (`id_pengguna`, `nama`, `alamat`, `no_telpon`, `email`
 
 CREATE TABLE `tb_peramalan` (
   `id_peramalan` int(11) NOT NULL,
-  `nama_barang` varchar(50) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `id_satuan` int(11) NOT NULL,
   `id_jenis` int(11) NOT NULL,
-  `id_satuan` int(11) NOT NULL
+  `minimal_stok` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `tb_peramalan`
 --
 
-INSERT INTO `tb_peramalan` (`id_peramalan`, `nama_barang`, `id_jenis`, `id_satuan`) VALUES
-(4, 'Yakult', 1, 9),
-(5, 'Aqua', 1, 9);
+INSERT INTO `tb_peramalan` (`id_peramalan`, `id_barang`, `id_satuan`, `id_jenis`, `minimal_stok`) VALUES
+(18, 21, 9, 1, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_permintaan`
+--
+
+CREATE TABLE `tb_permintaan` (
+  `id_permintaan` int(11) NOT NULL,
+  `id_peramalan` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `id_satuan` int(11) NOT NULL,
+  `jumlah_pengadaan` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_permintaan`
+--
+
+INSERT INTO `tb_permintaan` (`id_permintaan`, `id_peramalan`, `id_barang`, `id_satuan`, `jumlah_pengadaan`, `status`) VALUES
+(10, 17, 21, 9, 44, 'Sudah disetujui');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_produk`
+--
+
+CREATE TABLE `tb_produk` (
+  `id_produk` int(11) NOT NULL,
+  `id_pemasok` int(11) NOT NULL,
+  `nama_produk` varchar(50) NOT NULL,
+  `harga_produk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -217,17 +251,17 @@ CREATE TABLE `tb_stok_barang` (
   `kode_barang` varchar(10) NOT NULL,
   `nama_barang` varchar(50) NOT NULL,
   `id_jenis` int(11) NOT NULL,
-  `stok` int(11) NOT NULL,
-  `id_satuan` int(11) NOT NULL
+  `id_satuan` int(11) NOT NULL,
+  `stok` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `tb_stok_barang`
 --
 
-INSERT INTO `tb_stok_barang` (`id_barang`, `kode_barang`, `nama_barang`, `id_jenis`, `stok`, `id_satuan`) VALUES
-(12, 'BRG0001', 'Yakult', 1, 5, 9),
-(13, 'BRG0002', 'Aqua', 1, 50, 9);
+INSERT INTO `tb_stok_barang` (`id_barang`, `kode_barang`, `nama_barang`, `id_jenis`, `id_satuan`, `stok`) VALUES
+(21, 'BRG0001', 'Yakult', 1, 9, 5),
+(22, 'BRG0002', 'Aqua', 1, 9, 5);
 
 --
 -- Indexes for dumped tables
@@ -282,6 +316,18 @@ ALTER TABLE `tb_peramalan`
   ADD PRIMARY KEY (`id_peramalan`);
 
 --
+-- Indeks untuk tabel `tb_permintaan`
+--
+ALTER TABLE `tb_permintaan`
+  ADD PRIMARY KEY (`id_permintaan`);
+
+--
+-- Indeks untuk tabel `tb_produk`
+--
+ALTER TABLE `tb_produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
 -- Indeks untuk tabel `tb_satuan`
 --
 ALTER TABLE `tb_satuan`
@@ -301,13 +347,13 @@ ALTER TABLE `tb_stok_barang`
 -- AUTO_INCREMENT untuk tabel `tb_barang_keluar`
 --
 ALTER TABLE `tb_barang_keluar`
-  MODIFY `id_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+  MODIFY `id_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_barang_masuk`
 --
 ALTER TABLE `tb_barang_masuk`
-  MODIFY `id_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `id_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_jenis_barang`
@@ -325,7 +371,7 @@ ALTER TABLE `tb_kadaluarsa`
 -- AUTO_INCREMENT untuk tabel `tb_monitoring_kadaluarsa`
 --
 ALTER TABLE `tb_monitoring_kadaluarsa`
-  MODIFY `id_monitoring` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_monitoring` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_pemasok`
@@ -337,13 +383,25 @@ ALTER TABLE `tb_pemasok`
 -- AUTO_INCREMENT untuk tabel `tb_pengguna`
 --
 ALTER TABLE `tb_pengguna`
-  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_peramalan`
 --
 ALTER TABLE `tb_peramalan`
-  MODIFY `id_peramalan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_peramalan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_permintaan`
+--
+ALTER TABLE `tb_permintaan`
+  MODIFY `id_permintaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_produk`
+--
+ALTER TABLE `tb_produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_satuan`
@@ -355,7 +413,7 @@ ALTER TABLE `tb_satuan`
 -- AUTO_INCREMENT untuk tabel `tb_stok_barang`
 --
 ALTER TABLE `tb_stok_barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
